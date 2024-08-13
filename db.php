7c0e1be86a72cd5f4cf723797dec8f67735f9cc4
wanyKuznetsov <149588428+wanyKuznetsov@users.noreply.github.com>
2024-08-13 15:48:47 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => 'localhost',
+	'name' => 'test',
+	'user' => 'root',
+	'password' => '',
+];
---
+++ routes.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+	// MainController
+	'' => [
+		'controller' => 'main',
+		'action' => 'index',
+	],
+	'main/index/{page:\d+}' => [
+		'controller' => 'main',
+		'action' => 'index',
+	],
+
+
+
+
+	'support' => [
+		'controller' => 'main',
+		'action' => 'support',
+	],
+
+
+
+	
+	// AdminController
+	'admin/login' => [
+		'controller' => 'admin',
+		'action' => 'login',
+	],
+	'admin/logout' => [
+		'controller' => 'admin',
+		'action' => 'logout',
+	],
+	'admin/add' => [
+		'controller' => 'admin',
+		'action' => 'add',
+	],
+
+	
+// OtherController
+
+
+];
